cf6c699288178a19e186483ed74aca75feb467c8
ValentinVasilev <falioVasilev@gmail.com>
2022-06-04 18:42:25 +0300

    Product information section updated.

--- ecom/pages/products/[id].tsx
+++ ecom/pages/products/[id].tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouter } from "next/router";
 import ProductCarouselComponent from "../../components/product-carousel.component";
 import styles from '../../styles/product.module.scss';
+import { Rating } from "@mui/material";
 
 
 const Product = () => {
@@ -14,7 +15,20 @@ const Product = () => {
         <div className={styles.carouselContainer}>
           <ProductCarouselComponent />
         </div>
-        <div>2</div>
+        <div className={styles.productInfoContainer}>
+          <p className={styles.productTitle}>Product Title</p>
+          <div style={{ display: 'flex', marginTop: '-3vh' }}>
+            <div style={{ alignSelf: 'center' }}>
+              <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly />
+            </div>
+            <p style={{ alignSelf: 'center', fontFamily: 'Montserrat', color: 'gray' }}>(3 Reviews) </p>
+          </div>
+          <p style={{ color: 'green', fontFamily: 'Montserrat', fontSize: '25px', marginTop: '-3px', fontWeight: 'bold' }}>$$ Price tag</p>
+          <p>Quantity ={">"} Must be dropdown</p>
+          <div>
+            <button className={styles.addButton}>Add to Cart</button>
+          </div>
+        </div>
       </div>
     </div>
   )
