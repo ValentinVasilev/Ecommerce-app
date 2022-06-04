ed38652327ae09203de059c32a98dd51ace87f89
ValentinVasilev <falioVasilev@gmail.com>
2022-06-04 18:52:29 +0300

    Product information update.

--- ecom/pages/products/[id].tsx
+++ ecom/pages/products/[id].tsx
@@ -3,7 +3,12 @@ import { useRouter } from "next/router";
 import ProductCarouselComponent from "../../components/product-carousel.component";
 import styles from '../../styles/product.module.scss';
 import { Rating } from "@mui/material";
-
+import Image from "next/image";
+import FaceBookIcon from '../../assets/icons/facebook.png';
+import LinkedInIcon from '../../assets/icons/linkedin.png';
+import TwitterIcon from '../../assets/icons/twitter.png';
+import SkypeIcon from '../../assets/icons/skype.png';
+import InstagramIcon from '../../assets/icons/instagram.png';
 
 const Product = () => {
 
@@ -14,6 +19,23 @@ const Product = () => {
       <div className={styles.productWrapper}>
         <div className={styles.carouselContainer}>
           <ProductCarouselComponent />
+          <div className={styles.socialContainer}>
+            <div>
+              <Image src={FaceBookIcon} alt="facebook icon" width={40} height={40} />
+            </div>
+            <div>
+              <Image src={LinkedInIcon} alt="linkedin icon" width={40} height={40} />
+            </div>
+            <div>
+              <Image src={TwitterIcon} alt="twitter icon" width={40} height={40} />
+            </div>
+            <div>
+              <Image src={SkypeIcon} alt="skype icon" width={40} height={40} />
+            </div>
+            <div>
+              <Image src={InstagramIcon} alt="instagram icon" width={40} height={40} />
+            </div>
+          </div>
         </div>
         <div className={styles.productInfoContainer}>
           <p className={styles.productTitle}>Product Title</p>
