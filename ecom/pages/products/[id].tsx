9fc49d2be9a85cfee82a378506d88b0e9e570ca9
ValentinVasilev <falioVasilev@gmail.com>
2022-06-06 08:04:02 +0300

    Update [id].tsx

--- ecom/pages/products/[id].tsx
+++ ecom/pages/products/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import ProductCarouselComponent from "../../components/product-carousel.component";
 import styles from '../../styles/product.module.scss';
@@ -13,11 +13,14 @@ import ShieldIcon from '../../assets/icons/shield2.png';
 import AddedToWishlist from '../../assets/icons/added-to-wishlist.png'
 import AddToWishlist from '../../assets/icons/add-to-favorites.png'
 import CompareIcon from '../../assets/icons/compare.png';
+import { prepareServerlessUrl } from "next/dist/server/base-server";
 
 const Product = () => {
 
   const [isAdded, setIsAdded] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  // const [compareProducts, setCompareProducts] = useState<[]>([]);
+  let compareProducts = [''];
 
   const AddToWish = () => {
 
@@ -34,6 +37,15 @@ const Product = () => {
     }
 
   }
+
+  const addToCompare = () => {
+    // setCompareProducts(['1'])
+    compareProducts.push('test product');
+  }
+
+  useEffect(() => {
+    compareProducts
+  }, [compareProducts])
 
   const router = useRouter();
 
@@ -110,7 +122,7 @@ const Product = () => {
                 </button>
               </div>
               <div className={styles.comparePoduct}>
-                <button className={styles.compareBtn}>
+                <button className={styles.compareBtn} onClick={() => addToCompare()}>
                   <div style={{ alignSelf: 'center', paddingRight: '2vh', paddingLeft: '2vh' }}>
                     <Image src={CompareIcon} alt="compare icon" />
                   </div>
@@ -119,9 +131,25 @@ const Product = () => {
               </div>
             </div>
           </div>
+          <div>
+            <p>DESCRIPTION</p>
+            <p>
+              What is Lorem Ipsum?
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              Why do we use it?
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            </p></div>
+          {compareProducts.length <= 0 ? (<p>Nothing to compare!</p>) : (
+            <div>
+              <p>Compare Products:</p>
+              <div>{compareProducts.length}</div>
+              {compareProducts.map((item) => {
+                <div>{item}</div>
+              })}
+            </div>
+          )}
         </div >
       </div >
-      <div><p>123</p></div>
     </div >
   )
 }
