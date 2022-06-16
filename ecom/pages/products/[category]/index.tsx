203c3b14df98a855d6eca6a0ef7c7660407f8b4d
ValentinVasilev <falioVasilev@gmail.com>
2022-06-16 21:58:57 +0300

    Fix nested routes issue

--- ecom/pages/products/[category]/index.tsx
+++ ecom/pages/products/[category]/index.tsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
+import ProductsList from "../../../assets/data/products";
+import Link from "next/link";
 
 const Category = () => {
 
   const router = useRouter();
 
+  const productCategory = router.query.category;
+
   console.log(router)
+
   return (
-    <div>{router.query.category}</div>
+    <div>
+      {ProductsList
+        .filter(category => category.category === productCategory)
+        .map(product => {
+          return (
+            <div key={product.id}>
+              <p>{product.title}</p>
+              <Link href={
+                // `/products/${product.id || product["Product ID"]}`
+                {
+                  pathname: "category/[id]",
+                  query: {
+                    id: product.id,
+                  }
+                }
+              } as={`${productCategory}/${product.id}`} key={product.id}
+                passHref
+              >
+                <button>View More</button>
+              </Link>
+            </div>
+          )
+        })
+      }
+    </div>
   )
+
 }
 
 export default Category;
