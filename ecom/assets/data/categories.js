9318ff9cacab2b294fbcdea600fa490c50ed5e92
ValentinVasilev <falioVasilev@gmail.com>
2022-06-18 15:36:04 +0300

    Categories changes

--- ecom/assets/data/categories.js
+++ ecom/assets/data/categories.js
@@ -5,52 +5,64 @@ const categories = [
   {
     id: 1,
     title: 'smartphones',
-    img: ClotshImg
+    img: ClotshImg,
+    count: 5,
   },
   {
     id: 2,
     title: 'laptops',
-    img: LaptopsImg
+    img: LaptopsImg,
+    count: 5,
   },
   {
     id: 3,
     title: 'fragrances',
-    img: LaptopsImg
+    img: LaptopsImg,
+    count: 5,
   },
   {
     id: 4,
     title: 'skincare',
-    img: LaptopsImg
+    img: LaptopsImg,
+    count: 5,
   },
   {
     id: 5,
     title: 'groceries',
-    img: LaptopsImg
+    img: LaptopsImg,
+    count: 5,
   },
   {
     id: 6,
     title: 'home-decoration',
-    img: LaptopsImg
+    img: LaptopsImg,
+    count: 5,
   },
   {
     id: 7,
     title: 'shoes',
-    img: LaptopsImg
+    img: LaptopsImg,
+    count: 0,
   },
   {
     id: 8,
     title: 'electronics',
-    img: LaptopsImg
+    img: LaptopsImg,
+    count: 0,
+
   },
   {
     id: 9,
     title: 'furniture',
-    img: LaptopsImg
+    img: LaptopsImg,
+    count: 0,
+
   },
   {
     id: 10,
     title: 'cloths',
-    img: LaptopsImg
+    img: LaptopsImg,
+    count: 0,
   }
   // 'laptops',
   // 'fragrances',
--- ecom/pages/products/index.tsx
+++ ecom/pages/products/index.tsx
@@ -1,40 +1,23 @@
 import React, { useState } from "react";
-import Allproducts from '../../assets/data/products'
-import Image from "next/image";
-import Link from "next/link";
-import ProductCard from '../../components/product-card.component';
-import CategoryPanel from "../../components/category-panel.component";
 import categories from "../../assets/data/categories";
-import { Card, Grid, List, ListItem, ListItemText } from '@mui/material'
-import Tab from "../../components/sub-components/tabs.component";
 import ProductCategoryCard from "../../components/product-category-card";
-
 
 const Products = () => {
 
-  const [filterCategory, setFilterCategory] = useState(false);
-  let activeCategory = 'skincare';
-
-
-  const CategoryIsActive = (cat: string) => {
-    setFilterCategory(!filterCategory)
-    // activeCategory?.push(cat);
-    activeCategory = cat;
-    console.log(activeCategory)
-    // console.log(activeCategory[0])
-  }
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', width: '90%', justifyContent: 'center', alignItems: 'flex-start', backgroundColor: 'black' }}>
-      {categories.map(category => {
-        return (
-          <div key={category.id} >
-            <ProductCategoryCard
-              category={category.title}
-              categoryImage={category.img}
-            />
-          </div>
-        )
-      })}
+      {categories
+        .filter(categoryCount => categoryCount.count > 0)
+        .map(category => {
+          return (
+            <div key={category.id} >
+              <ProductCategoryCard
+                category={category.title}
+                categoryImage={category.img}
+              />
+            </div>
+          )
+        })}
     </div>
   )
 
