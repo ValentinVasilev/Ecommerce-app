3aba72a4a510ef4e0aa6eebd481dbfaf2c977b0e
ValentinVasilev <falioVasilev@gmail.com>
2022-06-18 19:31:32 +0300

    Register component completed.

--- ecom/components/sub-components/register.component.tsx
+++ ecom/components/sub-components/register.component.tsx
@@ -1,5 +1,5 @@
+import React, { useState } from "react";
 import { TextField } from "@mui/material";
-import React from "react";
 import styles from '../../styles/sub-components/register.component.module.scss';
 import { useRouter } from "next/router";
 
@@ -13,11 +13,24 @@ const Register = () => {
 
   const router = useRouter();
 
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+
   const RegisterUser = () => {
-    // alert('User successfully registered!')
-    setTimeout(() => {
-      router.push('/products')
-    }, 1000)
+
+    if (password != confirmPassword) {
+      alert('Password don`t match')
+    } else {
+      alert('User successfully registered!')
+
+      setTimeout(() => {
+        router.push('/products')
+      }, 1000)
+    }
+
+
   }
 
 
@@ -26,16 +39,36 @@ const Register = () => {
       <p className={styles.title}>Register your new account</p>
       <div className={styles.registerWrapper}>
         <div className={styles.inputFields}>
-          <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth />
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            fullWidth
+            onInput={(e) => setEmail((e.target as HTMLInputElement).value)}
+          />
         </div>
         <div className={styles.inputFields}>
-          <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth />
+          <TextField
+            type="password"
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            fullWidth
+            onInput={(e) => setPassword((e.target as HTMLInputElement).value)}
+          />
         </div>
         <div className={styles.inputFields}>
-          <TextField id="outlined-basic" label="Repeat Password" variant="outlined" fullWidth />
+          <TextField
+            type="password"
+            id="outlined-basic"
+            label="Repeat Password"
+            variant="outlined"
+            fullWidth
+            onInput={(e) => setConfirmPassword((e.target as HTMLInputElement).value)}
+          />
         </div>
-        <div className={styles.inputFields}>
-          <button onClick={() => RegisterUser()}>Register</button>
+        <div className={styles.buttonContainer}>
+          <button className={styles.registerButton} onClick={() => RegisterUser()}>Register</button>
         </div>
 
       </div>
