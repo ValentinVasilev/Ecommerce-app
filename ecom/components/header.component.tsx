ac22088ea5128de53d1235b35e171437b102bd5c
ValentinVasilev <falioVasilev@gmail.com>
2022-07-01 17:01:01 +0300

    Search component initially created

--- ecom/components/header.component.tsx
+++ ecom/components/header.component.tsx
@@ -45,7 +45,7 @@ const Header = () => {
                 <p style={{ fontSize: '35px', fontFamily: 'monospace' }}>M</p>
               </div>
             </Link>
-            <div style={{ display: 'flex', alignItems: 'center', marginBottom: '-10px', border: '1px solid red' }}>
+            <div style={{ display: 'flex', alignItems: 'center', marginBottom: '-10px' }}>
               <Search />
             </div>
             <div>Currency Dropdown</div>
@@ -62,28 +62,6 @@ const Header = () => {
           <div>Shoes</div>
         </div>
       </div>
-      {/* <div className={styles.headerWrapper}>
-        <div className={styles.leftSide}>
-          <Link href='/' passHref>
-            <p className={styles.storeTitle}>Store Name</p>
-          </Link>
-          <Link href='/products' passHref>
-            <p className={styles.titles}>Products</p>
-          </Link>
-
-          <p className={styles.titles}>About us</p>
-          <p className={styles.titles}>Portfolio</p>
-        </div>
-        <div className={styles.rightSide}>
-          <p className={styles.storePhone}>555-555-555</p>
-          <p style={{ alignSelf: 'center' }}>
-            {user ? (user?.email) : (null)}
-          </p>
-          <div style={{ alignSelf: 'center' }}>
-            <input placeholder="Search product" />
-          </div>
-        </div>
-      </div> */}
     </div >
   )
 }
--- ecom/components/sub-components/search.component.tsx
+++ ecom/components/sub-components/search.component.tsx
@@ -1,22 +1,75 @@
-import { InputBase, Paper } from "@mui/material";
-import React from "react";
+import { Button, Menu, MenuItem } from "@mui/material";
+import React, { useState } from "react";
+import Image from "next/image";
+import SearchIcon from '../../assets/icons/find.png';
+import styles from '../../styles/sub-components/search.component.module.scss';
+import UpArrow from '../../assets/icons/up-arrow.png';
+import DownArrow from '../../assets/icons/down-arrow.png';
+
 
 const Search = () => {
-  return (
-    <div>
-      <Paper
-        component="form"
-        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: '100%' }}
-      >
 
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Search"
-          inputProps={{ 'aria-label': 'search' }}
-        />
-      </Paper>
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div>
+        <input className={styles.input} placeholder="Search..." />
+      </div>
+      <SelectCategory />
+      <div>
+        <button>
+          <Image src={SearchIcon} alt="search icon" />
+        </button>
+      </div>
     </div>
   )
+}
+
+
+const SelectCategory = () => {
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        sx={{ backgroundColor: 'white', borderRadius: 0, padding: '7px 10px' }}
+      >
+        <b style={{ color: 'black', padding: '0px 5px 0px 5px' }}>Categories</b>
+        {open
+          ? (<Image src={UpArrow} alt="arrow up" width={16} height={16} />)
+          : (<Image src={DownArrow} alt="down up" width={16} height={16} />)}
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+        
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div >
+  );
 }
 
 export default Search;
