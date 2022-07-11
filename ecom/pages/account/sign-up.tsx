796919c58aaf13fdf89770cdd5a64fd0009c085e
ValentinVasilev <falioVasilev@gmail.com>
2022-07-11 11:46:16 +0300

    Sign up form initially completed.

--- ecom/pages/account/sign-up.tsx
+++ ecom/pages/account/sign-up.tsx
@@ -1,3 +1,4 @@
+import { Button, TextField } from "@mui/material";
 import React from "react";
 import styles from '../../styles/account/sign-up.module.scss';
 
@@ -8,8 +9,29 @@ const SignUp = () => {
       <div className={styles.formContainer}>
         <div className={styles.leftSide}>
           <p style={{ alignSelf: 'center', fontSize: '30px' }}>Sign up form</p>
+          <div className={styles.form}>
+            {/* <input placeholder="Email" className={styles.inputFields} /> */}
+            <TextField
+              variant="outlined"
+              sx={{ backgroundColor: 'transparent' }}
+              label="Email"
+              className={styles.inputFields2}
+            />
+            <TextField
+              variant="outlined"
+              sx={{ backgroundColor: 'transparent', margin: '5vh 0vh 5vh 0vh' }}
+              label="Password"
+              className={styles.inputFields2}
+            />
+            <TextField
+              variant="outlined"
+              sx={{ backgroundColor: 'transparent' }}
+              label="Repeat Password"
+              className={styles.inputFields2}
+            />
+          </div>
+          <Button variant="contained" style={{ width: '25%', alignSelf: 'center', margin: '4vh 0vh 0vh 0vh', backgroundColor: 'rgba(240, 46, 170, 0.2)', color: 'skyblue' }}>Sign Up</Button>
         </div>
-        <div>2</div>
       </div>
     </div>
   )
