2785342bcbdb3f333fcc22377e39d5a329d435ec
ValentinVasilev <falioVasilev@gmail.com>
2022-07-25 09:59:21 +0300

    Update layout.component.tsx

--- ecom/components/layout.component.tsx
+++ ecom/components/layout.component.tsx
@@ -4,6 +4,7 @@ import Header from '../components/header.component';
 import { Footer } from "./footer.component";
 import { useRouter } from 'next/router';
 
+
 type LayoutProps = {
   children?: React.ReactNode
 }
